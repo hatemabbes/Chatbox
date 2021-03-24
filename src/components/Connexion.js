c418097cfd6c77e9444d88d516e4d2651f6ba235
Hatem <hatem.aabbes@gmail.com>
2021-03-24 16:23:40 +0100

    Configure connexion component

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,18 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Redirect } from 'react-router-dom'
 
 class Connexion extends Component {
+    state = {
+        pseudo: '',
+        btnPressed: false
+    }
+
+    handleChange = event => {
+        const pseudo = event.target.value
+        this.setState({
+            pseudo
+        })
+    }
+    handleSubmit = event => {
+        event.preventDefault()
+        this.setState({
+            btnPressed: true
+        })
+    }
     render() {
+        if (this.state.btnPressed) {
+            return (<Redirect push to={`pseudo/${this.state.pseudo}`} />)
+        }
         return (
-            <div className="connexionBox">
-                <form className='connexion'>
-                    <input 
-                    type='text'
-                    required
-                    placeholder='Pseudo...'
-                    />
-                    <button type='submit'> Go ! </button>
-                </form>
-            </div>
+            <Fragment>
+                <div className='title'>
+                     <center> Welcome to chatbox  </center> 
+                </div>
+
+                <div className="connexionBox">
+                    <form className='connexion' onSubmit={this.handleSubmit}>
+                        <input
+                            type='text'
+                            required
+                            placeholder='Pseudo...'
+                            value={this.state.pseudo}
+                            onChange={this.handleChange}
+                        />
+                        <button type='submit'> Go ! </button>
+                    </form>
+                </div>
+            </Fragment>
+
         )
     }
 }
